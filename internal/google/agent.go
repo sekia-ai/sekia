74e794251d8f820a7be694122a3f218c8da4ec34
Julio Jimenez <julio@julioj.com>
2026-02-15 20:51:41 -0500

    chore(release): v0.0.17 (#55)
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- internal/google/agent.go
+++ internal/google/agent.go
@@ -19,7 +19,7 @@ import (
 
 const (
 	agentName    = "google-agent"
-	agentVersion = "0.0.16"
+	agentVersion = "0.0.17"
 )
 
 // GoogleAgent bridges Google services (Gmail, Calendar) to the sekia event bus
