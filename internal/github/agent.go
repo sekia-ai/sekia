6519c910e5376555795e8444c414977cb7294242
Julio Jimenez <julio@julioj.com>
2026-03-13 08:37:34 -0400

    chore(release): v0.1.6 (#101)
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- internal/github/agent.go
+++ internal/github/agent.go
@@ -21,7 +21,7 @@ import (
 
 const (
 	agentName    = "github-agent"
-	agentVersion = "0.1.5"
+	agentVersion = "0.1.6"
 )
 
 // GitHubAgent bridges GitHub webhooks and/or REST API polling to the sekia
--- internal/google/agent.go
+++ internal/google/agent.go
@@ -19,7 +19,7 @@ import (
 
 const (
 	agentName    = "google-agent"
-	agentVersion = "0.1.5"
+	agentVersion = "0.1.6"
 )
 
 // GoogleAgent bridges Google services (Gmail, Calendar) to the sekia event bus
